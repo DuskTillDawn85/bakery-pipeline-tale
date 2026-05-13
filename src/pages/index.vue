d4849d4e3da0ad128433cab156fd6b19fe40cc38
shawn <1067608727@qq.com>
2026-05-13 09:30:21 +0800

    init: 项目清零

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,9 @@
 <script setup>
-import { useRouter } from 'vue-router'
-import landingImage from '~/images/landing.png'
-
-const router = useRouter()
-
-const enterGame = () => {
-  router.push('/levels')
-}
 </script>
 
 <template>
-  <div class="w-full h-full cursor-pointer overflow-hidden" @click="enterGame">
-    <div class="relative w-full h-full">
-      <img :src="landingImage" alt="Bakery Pipeline Tale" class="w-full h-full object-cover">
-      <p class="text-2xl text-white absolute top-[82%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-center animate-pulse">
-        点击任意处开始游戏
-      </p>
-    </div>
+  <div class="w-full h-full overflow-hidden">
+    新建文件夹
   </div>
 </template>
 
